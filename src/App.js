c4a2289ac57cb69788e0167ddead13ffa0eb4458
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-02-26 13:49:47 +0100

    add user details popover link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Dashboard from './pages/dashboard/Dashboard';
 import Login from './pages/login/Login';
+import User from './pages/user/User';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/user/:id" element={<User />} />
         </Routes>
       </BrowserRouter>
     </div>
