54dfbbe09719d67e7596acd85ccda598707c5c38
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-02-26 10:39:47 +0100

    build filter component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -5,10 +5,8 @@ import SideNav from '../../components/sidenav/SideNav'
 import styles from './Dashboard.module.scss'
 import { userCardData } from '../../utility/data/userCardData'
 import Card from '../../components/card/Card'
-import filterIcon from '../../images/icons/filter_icon.png'
-import actionsIcon from '../../images/icons/actions_icon.png'
-import prevIcon from '../../images/icons/prev_btn.png'
-import nextIcon from '../../images/icons/next_btn.png'
+import Table from '../../components/table/Table'
+
 
 const tableHeadings = ['Organization', 'Username', 'Email', 'Phone Number', 'Date Joined', 'Status']
 
@@ -32,83 +30,7 @@ const Dashboard = () => {
                         }
                     </div>
                     {/* Table */}
-                    <div className={styles.table_container}>
-                        <table>
-                            <thead>
-                                <tr>
-                                    {
-                                        tableHeadings.map(heading => (
-                                            <th key={heading}>
-                                                <span style={{ marginRight: 4 }}>{heading}</span>
-                                                <span><img src={filterIcon} alt="" /></span>
-                                            </th>
-                                        ))
-                                    }
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Organization</td>
-                                    <td>Username</td>
-                                    <td>Email</td>
-                                    <td>Phone Number</td>
-                                    <td>Date Joined</td>
-                                    <td>Status</td>
-                                    <td>
-                                        <img src={actionsIcon} alt="" />
-                                    </td>
-
-                                </tr>
-                                <tr>
-                                    <td>Organization</td>
-                                    <td>Username</td>
-                                    <td>Email</td>
-                                    <td>Phone Number</td>
-                                    <td>Date Joined</td>
-                                    <td>Status</td>
-                                    <td>
-                                        <img src={actionsIcon} alt="" />
-                                    </td>
-
-                                </tr>
-                                <tr>
-                                    <td>Organization</td>
-                                    <td>Username</td>
-                                    <td>Email</td>
-                                    <td>Phone Number</td>
-                                    <td>Date Joined</td>
-                                    <td>Status</td>
-                                    <td>
-                                        <img src={actionsIcon} alt="" />
-                                    </td>
-
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    {/* table footer */}
-                    <div className={styles.table_footer}>
-                        <div>
-                            <span>Showing </span>
-                            <select>
-                                <option value="100" defaultValue>{100}</option>
-                            </select>
-                            {" "}<span>out of 100</span>
-                        </div>
-                        <div className={styles.table_nav}>
-                            <div>
-                                <img src={prevIcon} alt="" width="24px" />
-                            </div>
-                            <div>
-                                1 2 3 .... 16
-                            </div>
-                            <div>
-                                <img src={nextIcon} alt="" width="24px" />
-
-                            </div>
-
-                        </div>
-                    </div>
+                    <Table tableHeadings={tableHeadings} />
                 </div>
             </div>
 
