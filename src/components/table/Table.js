86feec4196ad6a8c52b65db33199ad621d6577e9
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-03-13 11:58:10 +0100

    add tablefooter component

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 import styles from './Table.module.scss'
 import filterIcon from '../../images/icons/filter_icon.png'
-import prevIcon from '../../images/icons/prev_btn.png'
-import nextIcon from '../../images/icons/next_btn.png'
 import Filter from '../filter/Filter'
 import TableRow from '../tablerow/TableRow'
+import TableFooter from '../tablefooter/TableFooter'
 
 const Table = ({ tableHeadings }) => {
     const [showFilter, setShowFilter] = useState(false)
@@ -58,7 +57,7 @@ const Table = ({ tableHeadings }) => {
             if (i === 3) {
                 break
             }
-            pages.push(<button className={styles.page_btn} onClick={() => setCurrentPage(`${Number(i + 1)}`)}>{i + 1}</button>)
+            pages.push(<button key={i * 100} className={styles.page_btn} onClick={() => setCurrentPage(`${Number(i + 1)}`)}>{i + 1}</button>)
         }
         return pages
     }
@@ -96,45 +95,14 @@ const Table = ({ tableHeadings }) => {
                 </table>
             </div>
             {/* table footer */}
-            <div className={styles.table_footer}>
-                <div>
-                    <span>Showing </span>
-                    <select onChange={handleChange}>
-                        <option value={10} defaultValue>{10}</option>
-                        <option value={25} >{25}</option>
-                        <option value={100} >{100}</option>
-                    </select>
-                    {" "}<span>out of {users.length}</span>
-                </div>
-                <div className={styles.table_nav}>
-                    <div>
-                        <img
-                            src={prevIcon}
-                            alt=""
-                            width="24px"
-                            onClick={() => currentPage > 1 && setCurrentPage(prev => Number(prev) - 1)}
-                        />
-                    </div>
-                    {
-                        totalPages >= 4 &&
-                        <div>
-                            {handlePageNumbers()}
-                            <span>....</span>
-                            <button className={styles.page_btn} onClick={() => setCurrentPage(totalPages)}>{totalPages}</button>
-                        </div>
-                    }
-                    {
-                        totalPages < 4 &&
-                        <div>{handlePageNumbers()}</div>
-                    }
-                    <div>
-                        <img src={nextIcon} alt="" width="24px"
-                            onClick={() => currentPage < totalPages && setCurrentPage(prev => Number(prev) + 1)}
-                        />
-                    </div>
-                </div>
-            </div>
-            <p className={styles.current_page}>{`page ${currentPage} of ${totalPages}`}</p>
+            <TableFooter
+                handleChange={handleChange}
+                currentPage={currentPage}
+                users={users}
+                setCurrentPage={setCurrentPage}
+                handlePageNumbers={handlePageNumbers}
+                totalPages={totalPages}
+            />
             {showFilter && <Filter />}
         </div>
     )
